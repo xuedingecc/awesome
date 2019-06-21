95aad1e072e664445fc879ba8454142388f2f382
chencheng <17625212308@163.com>
2019-06-21 16:06:48 +0800

    add orm.py

--- www/orm.py
+++ www/orm.py
@@ -48,4 +48,171 @@ def execute(sql, args):
 			raise
 		return affected
 
+def create_args_string(num):
+	L = []
+	for n in range(num):
+		L.append('?')
+	return ','.join(L)
 
+class Field(object):
+	def __init__(self, name, column_type, primary_key. default):
+		self.name = name
+		self.column_type = column_type
+		self.primary_key = primary_key
+		self.default = default
+
+	def __str__(self):
+		return '<%s, %s, %s>' % (self.__class__.__name__, self.column_type, self.name)
+
+class StringField(Field):
+	def __init__(self, name=None, primary_key=False, default=None, ddl='varchar(100)'):
+		super().__init__(name, ddl, primary_key, default)
+
+class BoolenField(Field):
+	def __init__(self, name=None, default=False):
+		super().__init__(name, 'boolean', False, default)
+
+class IntegerField(Field):
+	def __init__(self, name=None, primary_key=None, default=0):
+		super().__init__(name, 'bigint', primary_key, default)
+
+class FloatField(Field):
+	def __init__(self, name=None, primary_key=None, default=0.0):
+		super().__init__(name, 'real', primary_key, default)
+
+class TextField(Field):
+	def __init__(self, name=None, default=None):
+		super().__init__(name, 'text', False, default)
+
+class ModelMetaclass(type):
+	def __new__(cls, name, bases, attrs):
+		if name == 'Model':
+			return type.__new__(cls, name, bases, attrs)
+		tableName = attrs.get('__table__', None) or name
+		logging.info('found model: %s (table: %s)' % (name, tableName))
+		mappings = dict()
+		fields = []
+		primaryKey = None
+		for k, v in attrs.items():
+			if isinstance(v, Field):
+				logging.info(' found mapping: %s ==> %s' % (k, v))
+				mappings[k] = v
+				if v.primary_key:
+					if primaryKey:
+						raise RuntimeError('Duplicate primary key for field: %s' % k)
+					primaryKey = k
+				else:
+					field.append(k)
+		if not primaryKey:
+			raise RuntimeError('primary key not found.')
+		for k in mappings.keys():
+			attrs.pop(k)
+		escaped_fields = list(map(lambda f: '`%s`' % f, fields))
+		attrs['__mappings__'] = mappings
+		attrs['__table__'] = tableName
+		attrs['__primary_key__'] = primaryKey
+		attrs['__field__'] = field
+		attrs['__select__'] = 'select `%s`, %s from `%s`' % (primaryKey, ','.join(escaped_fields), tableName)
+		attrs['__insert__'] = 'insert into `%s` (%s, `%s`) valus (%s)' % (tableName, ','.join(escaped_fields), primaryKey, create_args_string(len(escaped_fields) + 1))
+		attrs['__update__'] = 'update `%s` set `%s` where `%s`=?' % (tableName, ','.join(map(lambda f: '`%s`=?' % (mappings.get(f).name or f), field)), primaryKey)
+		attrs['__delete__'] = 'delete from `%s` where `%s`=?' % (tableName, primaryKey)
+		return type.__new__(cls, name, bases, attrs)
+
+class Model(dict, metaclass=ModelMetaclass):
+	def __init__(self, **kw):
+		super(Model, self).__init__(**kw)
+
+	def __getattr__(self, key):
+		try:
+			return self[key]
+		except KeyError:
+			raise AttributeError(r"'Model' object has no attribute '%s'" % key)
+
+	def __setattr__(self, key, value):
+		self[key] = value
+
+	def getValue(self, key):
+		return getarttr(self, key, None)
+
+	def getValueOrDefault(self, key):
+		value = getarttr(self, key, None)
+		if value is None:
+			field = self.__mappings__[key]
+			if field.default is not None:
+				value = field.default() if callable(field.default) else field.default
+				logging.debug('using default value for %s: %s' % (key, str(value)))
+				setattr(self, key, value)
+		return value
+
+	@classmethod
+	@asyncio.coroutine
+	def find(cls, pk):
+		'find object by primary key.'
+		rs = yield from select('%s where `%s`=?' % (cls.__select__, cls.__primary_key__), [pk], 1)
+		if len(rs) == 0:
+			return None
+		return cls(**rs[0])
+
+	@classmethod
+	@asyncio.coroutine
+	def findNumber(cls, selectField, where=None, args=None):
+		' find number by select and where. '
+		sql = ['select %s _num_ from `%s`' % (selectField, cls.__table__)]
+		if where:
+			sql.append('where')
+			sql.append(where)
+		rs = yield from select(' '.join(sql), args, 1)
+		if len(rs) == 0:
+			return None
+		return re[0]['_num_']
+
+	@classmethod
+	@asyncio.coroutine
+	def findAll(cls, where=None, args=None, **kw):
+		'find object by where clause.'
+		sql = [cls.__select__]
+		if where:
+			sql.append('where')
+			sql.append(where)
+		if args is None:
+			args = []
+		orderBy = kw.get('orderBy', None)
+		if orderBy:
+			sql.append('order by')
+			sql.append(orderBy)
+		limit = kw.get('limit', None)
+		if limit:
+			sql.append('limit')
+			if isinstance(limit, int):
+				sql.append('?')
+				sql.append(limit)
+			elif isinstance(limit, tuple) and len(limit) == 2:
+				sql.append('?, ?')
+				sql.append(limit)
+			else:
+				raise ValueError('Invalid limit value: %s' % str(limit))
+		rs = yield from select(' '.join(sql), args)
+		return [cls(**r) for r in rs]
+
+	@asyncio.coroutine
+	def save(self):
+		args = list(map(self.getValueOrDefault, self.__field__))
+		args.append(self.getValueOrDefault(self.__primary_key__))
+		rows = yield from execute(self.__insert__, args)
+		if rows != 1:
+			logging.warn('faild to insert record: affected rows: %s' % rows)
+
+	@asyncio.coroutine
+	def update(self):
+		args = list(map(self.getValue, self.__field__))
+		args.append(self.getValue(self.__primary_key__))
+		rows = yield from execute(self.__update__, args)
+		if rows != 1:
+			logging.warn('faild to update record: affected rows: %s' % rows)
+
+	@asyncio.coroutine
+	def remove(self):
+		args = [self.getValue(self.__primary_key__)]
+		rows = yield from execute(self.__delete__, args)
+		if rows != 1:
+			logging.warn('faild to delete record: affected rows: %s' % rows)
