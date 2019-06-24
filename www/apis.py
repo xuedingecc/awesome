0b8d748f1091cb6b77bc47272066c383942bdd9f
chencheng <17625212308@163.com>
2019-06-24 18:22:46 +0800

    modify apis.py

--- www/apis.py
+++ www/apis.py
@@ -19,11 +19,20 @@ class APIValueError(APIError):
 	Indicate the input value has error or invalid. The data specifies the error field of input form.
 	'''
 	def __init__(self, field, message=''):
+		super(APIValueError, self).__init__('value:invalid', field, message)
+
+class APIResourceNotFoundError(APIError):
+	'''
+	Indicate the resource was not found. The data specifies the resource name.
+	'''
+	def __init__(self, field, message=''):
 		super(APIResourceNotFoundError, self).__init__('value:notfound', field, message)
-		
-class APIPermissionError(APIError):
+
+class APIPermissionError(object):
 	'''
 	Indicate the api has no permission.
 	'''
-	def __init__(self, message=''):
-		super(APIPermissionError, self)..__init__('permission:forbidden', 'permission', message)
+	def __init__(self, arg):
+		super(APIPermissionError, self).__init__()
+		self.arg = arg
+		
